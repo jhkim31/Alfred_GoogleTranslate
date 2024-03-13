faade68d3b503de92a9c692404c6ad5d11d7a17d
jhkim31 <biz.jh.kim@gmail.com>
2024-03-13 14:35:41 +0900

    feat: update translate script

--- Google-Translate/en2ko.sh
+++ Google-Translate/en2ko.sh
@@ -1,45 +1,25 @@
 #!/bin/zsh
-# Alfred Script Filter JSON format
-#
-# This example demonstrates all fields available for populating results.
-#
-# For an in-depth explanation, use the (?) help button to the bottom left.
-#ㅊ
-str={query}
-q=${str// /%20}
-cat {query}
-cat $str
-cat $q
+str=$@
 
-translateText=$(curl https://translation.googleapis.com/language/translate/v2\?key\={your-api-key}\&q\=$q\&source\=en\&target\=ko\&format\=text | /opt/homebrew/bin/jq '.data.translations[0].translatedText')
+q=${str//\"/\'}
+q=$(echo "$q" | tr '\n' ' ')
+
+
+url=https://translation.googleapis.com/language/translate/v2
+translateText=$(curl --data-urlencode "key={your-api-key}" --data-urlencode "q=$q" --data-urlencode "source=en" --data-urlencode "target=ko" --data-urlencode "format=text" $url | /opt/homebrew/bin/jq '.data.translations[0].translatedText')
 t=${translateText//\"/}
+
 cat << EOB
 {"items": [
-
 	{
-		"valid": true,
-		"uid": "desktop",
-		"type": "file",
-		"title": "$t",
-		"subtitle": "cmd : (한글, 영어), ctrl : 웹으로 확인",
-		"arg": "$t",
+		"title": "${t}",
+		"arg": "${t}",
 		"mods": {
-			"alt": {
-				"valid": true,
-				"arg": "$t",
-				"subtitle": "한글만 저장합니다 ex) hello"
-			},
 			"cmd": {
 				"valid": true,
-				"arg": "$str, $t",
-				"subtitle": "영어와 한글을 저장합니다 ex) hello, 안녕하세요"
-			},
-			"ctrl": {
-				"valid": true,
-				"arg": "$str",
-				"subtitle": "웹으로 확인합니다. : $str"
-			},
-
+				"arg": "$q, \"$t\"",
+				"subtitle": "사전에 저장합니다. ($q, \"$t\")"
+			}
 		}
 	}
 ]}
--- Google-Translate/ko2en.sh
+++ Google-Translate/ko2en.sh
@@ -1,40 +1,25 @@
 #!/bin/zsh
-# Alfred Script Filter JSON format
-#
-# This example demonstrates all fields available for populating results.
-#
-# For an in-depth explanation, use the (?) help button to the bottom left.
-#ㅊ
-str={query}
-q=${str// /%20}
-cat {query}
-cat $str
-cat $q
+str=$@
 
-translateText=$(curl https://translation.googleapis.com/language/translate/v2\?key\={your-api-key}\&q\=$q\&source\=ko\&target\=en\&format\=text | /opt/homebrew/bin/jq '.data.translations[0].translatedText')
+q=${str//\"/\`}
+q=$(echo "$q" | tr '\n' ' ')
+
+
+url=https://translation.googleapis.com/language/translate/v2
+translateText=$(curl --data-urlencode "key={your-api-key}" --data-urlencode "q=$q" --data-urlencode "source=ko" --data-urlencode "target=en" --data-urlencode "format=text" $url | /opt/homebrew/bin/jq '.data.translations[0].translatedText')
 t=${translateText//\"/}
+
 cat << EOB
 {"items": [
-
 	{
-		"valid": true,
-		"uid": "desktop",
-		"type": "file",
-		"title": "$t",
-		"subtitle": "cmd : 영어한글, ctrl : 웹으로 확인",
-		"arg": "$t",
+		"title": "${t}",
+		"arg": "${t}",
 		"mods": {
 			"cmd": {
 				"valid": true,
-				"arg": "$str, $t",
-				"subtitle": "한글과 영어를 저장합니다 ex) 안녕하세요, hello"
-			},
-			"ctrl": {
-				"valid": true,
-				"arg": "$str",
-				"subtitle": "웹으로 확인합니다. : $str"
-			},
-
+				"arg": "$q, \"$t\"",
+				"subtitle": "사전에 저장합니다. ($q, \"$t\")"
+			}
 		}
 	}
 ]}
